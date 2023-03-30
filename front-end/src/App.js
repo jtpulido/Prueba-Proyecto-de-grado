d9a86b1bde088aa63b9eba644e170bc42b43e09f
jtpulido <jtpulido@unbosque.edu.co>
2023-03-30 13:23:15 -0500

    Front

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import { Login } from './pages/login/Login';
+import { Recuperar2 } from "./pages/login/ventanas/recuperar contrasena2/Recuperar2"
+
+function App() {
+  return (
+    <div className="App">
+
+      <Login />
+
+    </div>
+  );
+}
+
+export default App;
